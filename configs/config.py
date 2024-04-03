24c41cb19da47bc063ce52db76216c1f0a239f43
103426463 <tranlongvu007@gmail.com>
2024-04-03 17:44:59 +0700

    push

--- configs/config.py
+++ configs/config.py
@@ -1,24 +1,14 @@
-BATCH_SIZE = 32
-LOSS = ""
-LEARNING_RATE = 0.001
-NO_EPOCHS = 2
-DROPOUT = False
-OPTIMIZER = "adam"
-
 
 MODEL_BACKBONE= "rn18"
 MODEL_NAME = 'rn18fas'
 ATTACK_TYPE = 'printing'
 INFERENCE_DEVICE = 'CUDA'
-TRAINING_DEVICE = 'CUDA'
 
-INFERENCE_TYPE = 'live'
+INFERENCE_TYPE = 'printing'
 
-TRAIN_DATASET = 'CVPR23'
 VAL_DATASET = 'CVPR23'
 TEST_DATASET = 'HAND_CRAWL'
 
-TRAIN_FROM_CHECKPOINT = False # pth
 TEST_FROM_CHECKPOINT = False # onnx
 
 PATH_TO_CHECKPOINT_MODEL = 'checkpoints/fas-best.pth'
@@ -27,9 +17,6 @@ PATH_TO_IMAGES = ''
 PATH_TO_VIDEOS = ''
 PATH_TO_SINGLE_VIDEO = './data/video_benchmark/0/real.mp4'
 
-PATH_TO_PRINTING_DATASET = 'data/datasets/CVPR23/train/'
-
-PATH_TO_TRAIN_DATASET = 'data/datasets/CVPR23/train/'
 
 PATH_TO_PRINTING_TEST_DATASET = 'data/datasets/Hand_crawl/images/'
 PATH_TO_REPLAY_TEST_DATA = 'data/datasets/Hand_crawl/videos/'
